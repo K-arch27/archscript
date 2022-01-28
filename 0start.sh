c827f1217bf4bc5f72a6c7f1f15995a56bccfa0d
K-arch27 <98610690+K-arch27@users.noreply.github.com>
2022-01-28 13:46:35 -0500

    Update 0start.sh

--- 0start.sh
+++ 0start.sh
@@ -1,8 +1,8 @@
 # Find the name of the folder the scripts are in
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 clear
-mkdir /k-archscript
-cp -R ${SCRIPT_DIR} /k-archscript
+mkdir /archscript
+cp -R ${SCRIPT_DIR} /archscript
 echo -ne "
 -------------------------------------------------------------------------
                  █████╗ ██████╗  ██████╗██╗  ██╗
@@ -16,15 +16,15 @@ echo -ne "
 -------------------------------------------------------------------------
 
 "
-    ( bash /k-archscript/1setup.sh )|& tee startup.log
-    source /k-archscript/setup.conf
-    ( bash /k-archscript/2partition.sh )|& tee partition.log
-    ( bash /k-archscript/3strap.sh )|& tee strap.log
-    ( arch-chroot /mnt /root/k-archscript/4chroot.sh )|& tee chroot.log
-    ( arch-chroot /mnt /root/k-archscript/5final.sh )|& tee chroot.log
+    ( bash /archscript/1setup.sh )|& tee startup.log
+    source /archscript/setup.conf
+    ( bash /archscript/2partition.sh )|& tee partition.log
+    ( bash /archscript/3strap.sh )|& tee strap.log
+    ( arch-chroot /mnt /root/archscript/4chroot.sh )|& tee chroot.log
+    ( arch-chroot /mnt /root/archscript/5final.sh )|& tee chroot.log
     #( arch-chroot /mnt /usr/bin/runuser -u $USERNAME -- /home/$USERNAME/ArchTitus/2-user.sh )|& tee 2-user.log
     #( arch-chroot /mnt /root/ArchTitus/3-post-setup.sh )|& tee 3-post-setup.log
-    cp *.log /mnt/root/k-archscript/*.log
+    cp *.log /mnt/root/archscript/*.log
 echo -ne "
 -------------------------------------------------------------------------
                  █████╗ ██████╗  ██████╗██╗  ██╗
