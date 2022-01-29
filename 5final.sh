f131ef486d7962481fada754ed5d80d662c89271
K-arch27 <98610690+K-arch27@users.noreply.github.com>
2022-01-28 20:54:09 -0500

    Update 5final.sh

--- 5final.sh
+++ 5final.sh
@@ -19,7 +19,7 @@ echo -ne "
                     Setup hostname and timezone
 -------------------------------------------------------------------------
 "
-hostnamectl set-hostname $NAME_OF_MACHINE
+$NAME_OF_MACHINE > /etc/hostname
 timedatectl --no-ask-password set-timezone $TIMEZONE
 timedatectl --no-ask-password set-ntp 1
 localectl --no-ask-password set-locale LANG="en_CA.UTF-8" LC_TIME="en_CA.UTF-8"
@@ -41,6 +41,9 @@ pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
 pacman-key --lsign-key FBA220DFC880C036
 pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 cat /root/archscript/mirror.txt >> /etc/pacman.conf
+curl -O https://blackarch.org/strap.sh
+chmod +x strap.sh
+bash strap.sh
 pacman -Syyu --noconfirm
-pacman -S paru octopi snap-pac-grub --noconfirm
+pacman -S paru octopi snap-pac-grub stacer nerd-fonts-fantasque-sans-mono --noconfirm
 
