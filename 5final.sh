ec44d88a3328cbc0c746a09ef0988730ce5dee14
K-arch27 <98610690+K-arch27@users.noreply.github.com>
2022-01-29 18:26:26 -0500

    Update 5final.sh

--- 5final.sh
+++ 5final.sh
@@ -30,7 +30,7 @@ echo -ne "
 
 pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
 pacman-key --lsign-key FBA220DFC880C036
-pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' --noconfirm
 cat /root/archscript/mirror.txt >> /etc/pacman.conf
 curl -O https://blackarch.org/strap.sh
 chmod +x strap.sh
