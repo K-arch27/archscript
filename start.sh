67c653bb6253e6333bfdb67fb1709d9a699d533b
K-arch27 <98610690+K-arch27@users.noreply.github.com>
2022-01-29 18:32:00 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -28,10 +28,8 @@ echo -ne "
     ( bash /archscript/3strap.sh )|& tee ${SCRIPT_DIR}/log/strap.log
     ( arch-chroot /mnt /root/archscript/4chroot.sh )|& tee ${SCRIPT_DIR}/log/chroot.log
     ( arch-chroot /mnt /root/archscript/5final.sh )|& tee ${SCRIPT_DIR}/log/final.log
-    cp -R ${SCRIPT_DIR} mnt/home/$USERNAME/Desktop/logs
-    touch mnt/home/$USERNAME/Desktop/logs/IMPORTANT.txt
-    echo "please delete this folder and the one inside /root/archscript , or move them to a secure location, since they contains trace of your installation (like your password and username) " > mnt/home/$USERNAME/Desktop/logs/IMPORTANT.txt
-    
+   
+   
 echo -ne "
 -------------------------------------------------------------------------
                  █████╗ ██████╗  ██████╗██╗  ██╗
