4339b3a223847220417f71d6e93dc175eea54e7f
K-arch27 <98610690+K-arch27@users.noreply.github.com>
2022-01-28 19:44:57 -0500

    Update 5final.sh

--- 5final.sh
+++ 5final.sh
@@ -41,7 +41,6 @@ pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
 pacman-key --lsign-key FBA220DFC880C036
 pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 cat /root/archscript/mirror.txt >> /etc/pacman.conf
-bash black.sh
 pacman -Syyu --noconfirm
 pacman -S paru octopi snap-pac-grub --noconfirm
 
