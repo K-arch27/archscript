5bf4eb3f76f440e8157041b2a0ef85dffb548088
K-arch27 <98610690+K-arch27@users.noreply.github.com>
2022-01-28 19:51:37 -0500

    Update 0start.sh

--- 0start.sh
+++ 0start.sh
@@ -29,10 +29,7 @@ echo -ne "
     ( bash /archscript/3strap.sh )|& tee strap.log
     ( arch-chroot /mnt /root/archscript/4chroot.sh )|& tee chroot.log
     ( arch-chroot /mnt /root/archscript/5final.sh )|& tee final.log
-    ( arch-chroot /mnt /root/archscript/black.sh )|& tee black.log
-    #( arch-chroot /mnt /usr/bin/runuser -u $USERNAME -- /home/$USERNAME/ArchTitus/2-user.sh )|& tee 2-user.log
-    #( arch-chroot /mnt /root/ArchTitus/3-post-setup.sh )|& tee 3-post-setup.log
-    cp *.log /mnt/root/archscript/*.log
+    
 echo -ne "
 -------------------------------------------------------------------------
                  █████╗ ██████╗  ██████╗██╗  ██╗
