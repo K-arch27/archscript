c4c68fb7d0ce88ff41ddaf6e1f9e486777c07259
K-arch27 <98610690+K-arch27@users.noreply.github.com>
2022-01-30 20:05:05 -0500

    Update 6log.sh

--- 6log.sh
+++ 6log.sh
@@ -1,3 +1,7 @@
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+source /root/archscript/config.sh
+
+
  cp -R ${SCRIPT_DIR} mnt/home/$USERNAME/Desktop/logs
     touch mnt/home/$USERNAME/Desktop/logs/IMPORTANT.txt
     echo "please delete this folder and the one inside /root/archscript, 
