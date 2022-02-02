976892820da944cb4242bd154ac7c76598d3daed
K-arch27 <98610690+K-arch27@users.noreply.github.com>
2022-02-02 13:48:39 -0500

    Update 4chroot.sh

--- 4chroot.sh
+++ 4chroot.sh
@@ -22,27 +22,8 @@ sed -i "s/^#${LANGLOCAL}/${LANGLOCAL}/" /etc/locale.gen
 locale-gen
 
 
-# Add sudo no password rights
-if [ -z "$1" ]; then
-
-  # When you run the script, you will run this block since $1 is empty.
-
-  echo "Starting up visudo with this script as first parameter"
-
-  # We first set this script as the EDITOR and then starts visudo.
-  # Visudo will now start and use THIS SCRIPT as its editor
-  export EDITOR=$0 && sudo -E visudo
-else
-
-  # When visudo starts this script, it will provide the name of the sudoers 
-  # file as the first parameter and $1 will be non-empty. Because of that, 
-  # visudo will run this block.
-
-  echo "Changing sudoers"
-
-  # We change the sudoers file and then exit  
-  echo "%wheel ALL=(ALL:ALL) ALL" >> $1
-fi
+# Add sudo rights
+sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
 
 
 #Add parallel downloading
