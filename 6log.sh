379c24b9fe727429d3d2c219bf88d41ee0b4287d
K-arch27 <98610690+K-arch27@users.noreply.github.com>
2022-02-02 14:28:44 -0500

    Update 6log.sh

--- 6log.sh
+++ 6log.sh
@@ -2,8 +2,8 @@ SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 source /root/archscript/config.sh
 
  mkdir /home/$USERNAME/logs
- cp -R ${SCRIPT_DIR} /home/$USERNAME/Desktop/logs
- touch /home/$USERNAME/Desktop/logs/IMPORTANT.txt
+ cp -R ${SCRIPT_DIR} /home/$USERNAME/logs
+ touch /home/$USERNAME/logs/IMPORTANT.txt
  echo "please delete this folder and the one inside /root/archscript, 
        or move them to a secure location, 
        since they contains trace of your installation (like your password and username) " > /home/$USERNAME/logs/IMPORTANT.txt
