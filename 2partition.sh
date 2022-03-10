bc9b27455d68adaf1f48ec064e6364b39a8e0772
K-arch27 <98610690+K-arch27@users.noreply.github.com>
2022-03-09 23:42:46 -0500

    Update 2partition.sh

--- 2partition.sh
+++ 2partition.sh
@@ -17,8 +17,6 @@ source /archscript/config.sh
 	btrfs subvolume create /mnt/@/root
 	btrfs subvolume create /mnt/@/srv
 	btrfs subvolume create /mnt/@/tmp
-	mkdir /mnt/@/usr
-	btrfs subvolume create /mnt/@/usr/local
 	mkdir /mnt/@/var
 	btrfs subvolume create /mnt/@/var/cache
 	btrfs subvolume create /mnt/@/var/log
@@ -46,7 +44,6 @@ source /archscript/config.sh
 	mkdir -p /mnt/boot/grub
 	mkdir /mnt/root
 	mkdir /mnt/tmp
-	mkdir -p /mnt/usr/local
 	mkdir -p /mnt/var/cache
 	mkdir /mnt/var/log
 	mkdir /mnt/var/spool
@@ -61,7 +58,6 @@ source /archscript/config.sh
     mount UUID=${ROOTUUID} -o noatime,compress=zstd,ssd,commit=120,subvol=@/boot/grub /mnt/boot/grub
     mount UUID=${ROOTUUID} -o noatime,compress=zstd,ssd,commit=120,subvol=@/root /mnt/root
     mount UUID=${ROOTUUID} -o noatime,compress=zstd,ssd,commit=120,subvol=@/tmp /mnt/tmp
-    mount UUID=${ROOTUUID} -o noatime,compress=zstd,ssd,commit=120,subvol=@/usr/local /mnt/usr/local
     mount UUID=${ROOTUUID} -o noatime,ssd,commit=120,subvol=@/var/cache /mnt/var/cache
     mount UUID=${ROOTUUID} -o noatime,ssd,commit=120,subvol=@/var/log,nodatacow /mnt/var/log
     mount UUID=${ROOTUUID} -o noatime,ssd,commit=120,subvol=@/var/spool,nodatacow /mnt/var/spool
