ca6e8ba2f0eb3c0442ceb896e68fb6407d89115f
K-arch27 <98610690+K-arch27@users.noreply.github.com>
2023-03-14 12:53:11 -0400

    Update 6log.sh

--- 6log.sh
+++ 6log.sh
@@ -1,5 +1,6 @@
+#!/usr/bin/env bash
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-source /root/archscript/config.sh
+source $SCRIPT_DIR/config.sh
 
  mkdir /home/$USERNAME/logs
  cp -R ${SCRIPT_DIR} /home/$USERNAME/logs
