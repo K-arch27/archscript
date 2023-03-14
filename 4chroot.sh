d88563447924926df9bf6f7eeec1b4ae1f1db767
K-arch27 <98610690+K-arch27@users.noreply.github.com>
2023-03-14 12:52:30 -0400

    Update 4chroot.sh

--- 4chroot.sh
+++ 4chroot.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-source /root/archscript/config.sh
+source $SCRIPT_DIR/config.sh
+
 clear
 logo
 echo -ne "
