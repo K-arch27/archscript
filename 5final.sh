344c95f8480726aec8c601bed2caa8b0b0e26344
K-arch27 <98610690+K-arch27@users.noreply.github.com>
2023-03-14 12:52:45 -0400

    Update 5final.sh

--- 5final.sh
+++ 5final.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-source /root/archscript/config.sh
+source $SCRIPT_DIR/config.sh
 
 logo
 echo -ne "
