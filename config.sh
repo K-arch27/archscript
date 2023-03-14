4abd4ccd3a5a73c783b77297360a8325d133bf74
K-arch27 <98610690+K-arch27@users.noreply.github.com>
2023-03-14 19:01:50 -0400

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,9 +1,4 @@
-set_option() {
-    if grep -Eq "^${1}.*" $CONFIG_FILE; then # check if option exists
-        sed -i -e "/^${1}.*/d" $CONFIG_FILE # delete option if exists
-    fi
-    echo "${1}=${2}" >>$CONFIG_FILE # add option
-}
+
 
 select_option() {
 
