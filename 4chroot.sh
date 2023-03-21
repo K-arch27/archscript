3607c9db3318e30a77986b1e3b2b39be35c93bfd
K-arch27 <98610690+K-arch27@users.noreply.github.com>
2023-03-21 19:41:52 +0000

    Update 4chroot.sh

--- 4chroot.sh
+++ 4chroot.sh
@@ -20,7 +20,16 @@ sed -i 's/^#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
 
 #Enable multilib
 sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
-pacman -Sy --noconfirm
+
+#Installing some shit
+
+pacman -Syu --needed --noconfirm qemu-desktop libvirt edk2-ovmf virt-manager dnsmasq
+pacman -S --needed nvidia-dkms nvidia-utils lib32-nvidia-utils nvidia-settings vulkan-icd-loader lib32-vulkan-icd-loader
+pacman -S --needed lib32-mesa vulkan-intel lib32-vulkan-intel vulkan-icd-loader lib32-vulkan-icd-loader go
+systemctl enable libvirtd.service
+systemctl enable virtlogd.socket
+virsh net-autostart default
+
 clear
 logo
 echo -ne "
