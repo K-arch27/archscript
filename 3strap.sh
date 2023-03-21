43f23cc7ddf44bbcc20b146550b0e907d927aa70
K-arch27 <98610690+K-arch27@users.noreply.github.com>
2023-03-21 18:59:27 +0000

    Update 3strap.sh

--- 3strap.sh
+++ 3strap.sh
@@ -29,13 +29,11 @@ echo -ne "
 
 
       pacstrap /mnt base base-devel linux-firmware e2fsprogs dosfstools grub grub-btrfs os-prober efibootmgr btrfs-progs ntfs-3g snapper snap-pac fish linux-zen linux-zen-headers linux-lts linux-lts-headers networkmanager sof-firmware man-db man-pages texinfo 
-
-
-
-#GUI choice installation
-if [ "$DECHOICE" = "kaidaplasma" ]; then
+      
       pacstrap /mnt  xorg xorg-server xorg-xinit alacritty dolphin dolphin-plugins ark kate kcalc kolourpaint spectacle krunner partitionmanager packagekit-qt5 lxqt breeze-icons
+      
       pacstrap /mnt latte-dock discord htop kruler ksysguard nano starship neofetch firefox git
+      
       pacstrap /mnt noto-fonts-emoji noto-fonts-extra ttf-nerd-fonts-symbols zip unrar arch-install-scripts intel-ucode
 
 
