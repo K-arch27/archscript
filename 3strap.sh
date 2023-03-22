17190c845d981214746b2f416d682e1756a41bcf
K-arch27 <98610690+K-arch27@users.noreply.github.com>
2023-03-21 20:07:19 -0400

    Update 3strap.sh

--- 3strap.sh
+++ 3strap.sh
@@ -30,16 +30,21 @@ echo -ne "
 
       pacstrap /mnt base base-devel linux-firmware e2fsprogs dosfstools grub grub-btrfs os-prober efibootmgr btrfs-progs ntfs-3g snapper snap-pac fish linux-zen linux-zen-headers linux-lts linux-lts-headers networkmanager sof-firmware man-db man-pages texinfo 
       
-      pacstrap /mnt  xorg xorg-server xorg-xinit alacritty dolphin dolphin-plugins ark kate kcalc kolourpaint spectacle krunner partitionmanager packagekit-qt5 lxqt breeze-icons
+      pacstrap /mnt  xorg xorg-server xorg-xinit alacritty dolphin dolphin-plugins ark kate kcalc kolourpaint spectacle krunner partitionmanager plasma-desktop
       
-      pacstrap /mnt latte-dock discord htop kruler ksysguard nano starship neofetch firefox git
+      pacstrap /mnt latte-dock discord htop kruler ksysguard nano starship neofetch firefox git thefuck
       
-      pacstrap /mnt noto-fonts-emoji noto-fonts-extra ttf-nerd-fonts-symbols zip unrar arch-install-scripts intel-ucode
+      pacstrap /mnt noto-fonts-emoji noto-fonts-extra ttf-nerd-fonts-symbols-common zip unrar arch-install-scripts intel-ucode
 
 
 
 
 clear
+
+mkdir /mnt/MyData
+mkdir /mnt/MyGames
+mount /dev/sda1 /mnt/MyData
+mount /dev/sdb1 /mnt/MyGames
 
 genfstab -U /mnt >> /mnt/etc/fstab
 
